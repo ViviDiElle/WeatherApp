5107aeee241838ed2afb3634597039e86f68ef31
ViviDiElle <dileonardov@yahoo.com>
2023-10-17 13:31:54 +0200

    First commit WeatherApp

---
+++ src/index.js
@@ -0,0 +1,38 @@
+let weather = {
+  "paris": {
+    temp: 19.7,
+    humidity: 80
+  },
+  "tokyo": {
+    temp: 17.3,
+    humidity: 50
+  },
+  "lisbon": {
+    temp: 30.2,
+    humidity: 20
+  },
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100
+  },
+  "oslo": {
+    temp: -5,
+    humidity: 20
+  }
+};
+
+let city = prompt("Enter a city");
+
+// Converti l'input dell'utente in minuscolo
+city = city.toLowerCase();
+
+// Controlla se la città esiste nell'oggetto weather
+if (weather.hasOwnProperty(city)) {
+    // Se la città esiste, mostra un avviso con le informazioni meteo
+    let temp = Math.round(weather[city].temp);
+    let humidity = Math.round(weather[city].humidity);
+    alert(`It is currently ${temp}°C in ${city} with a humidity of ${humidity}%`);
+} else {
+    // Se la città non esiste, mostra un avviso con un link a Google Weather
+    alert(`Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`);
+};
